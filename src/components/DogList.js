7126ce6c7f24ec507bb324f72b369b09a3a428b3
apavlushina <ssemina7@gmail.com>
2019-10-22 15:17:53 +0200

    steps 1-3

---
+++ src/components/DogList.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+export default class DogsList extends Component {
+  state = {
+    dogBreeds: null
+  };
+
+  componentDidMount() {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then(res => res.json())
+      .then(data => {
+        const breeds = Object.keys(data.message);
+        this.updateBreeds(breeds);
+      })
+      .catch(console.error);
+  }
+
+  updateBreeds(breeds) {
+    this.setState({
+      dogBreeds: breeds
+    });
+  }
+
+  render() {
+    return (
+      <div className="dogs-list">
+        <h1>Dogs List</h1>
+        {this.state.dogBreeds === null && "Loading..."}
+        {this.state.dogBreeds !== null && (
+          <ul>
+            {this.state.dogBreeds.map(breed => (
+              <li key={breed}>{breed}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
